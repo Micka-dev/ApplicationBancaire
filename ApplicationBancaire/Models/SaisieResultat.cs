87622138ba43b57a2931641c6c8d25677307dbcd
Mickaël Douceré <mickamickadou@gmail.com>
2025-06-02 16:10:46 +0200

    Ajout du modele SaisieResultat

---
+++ ApplicationBancaire/Models/SaisieResultat.cs
@@ -0,0 +1,11 @@
+﻿namespace ApplicationBancaire.Models
+{
+    // Classe pour encapsuler le résultat de saisie
+    public class SaisieResultat
+    {
+        public bool Abandon { get; set; } // Indique si l'utilisateur a abandonné
+        public string? Valeur { get; set; } // La saisie de l'utilisateur
+        public string? Message { get; set; } // Message complémentaire
+    }
+
+}
