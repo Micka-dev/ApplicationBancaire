746b66101413a58a60bda43a1b7a5a12f6fd428b
Mickaël Douceré <mickamickadou@gmail.com>
2025-06-02 16:09:54 +0200

    Ajout du modele MontantResultat

---
+++ ApplicationBancaire/Models/MontantResultat.cs
@@ -0,0 +1,9 @@
+﻿namespace ApplicationBancaire.Models
+{
+    public class MontantResultat
+    {
+        public bool Abandon { get; set; } // Indique si l'utilisateur a abandonné
+        public decimal? Montant { get; set; } // Le montant saisi par l'utilisateur
+    }
+
+}
